d717e1ebfaa5df406d9e1874d301daa21b56fcae
Ugenteraan Manogaran <m.ugenteraan15@gmail.com>
2024-05-17 01:32:10 +0800

    random masking strategy

--- models/mae.py
+++ models/mae.py
@@ -73,20 +73,49 @@ class MaskedAutoEncoder(nn.Module):
         self.apply(self.initialize_weights)
 
 
-    def initialize_weights(self):
+    def initialize_weights(self, m):
         '''Initialize the parameters.
         '''
         
         #initialize gaussian distribution on the weight parameters. Bias parameters will be 0 since some of them are initialized with 0 already.
+        
+        
+        if isinstance(m, nn.Linear):
+            torch.nn.init.xavier_uniform_(m.weight, std=self.std_init)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+        elif isinstance(m, nn.Conv2d):
+            torch.nn.init.xavier_uniform_(m.weight)
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+
         torch.nn.init.trunc_normal_(self.cls_token.weights, std=self.init_std)
         torch.nn.init.trunc_normal_(self.encoder_pos_embed.weights, std=self.init_std)
         torch.nn.init.trunc_normal_(self.mask_token.weights, std=self.init_std)
         torch.nn.init.trunc_normal_(self.decoder_pos_embed.weights, std=self.init_std)
-        
-        torch.nn.init.xavier_uniform_(self.patch_projection.weights) 
+    
 
+    def random_masking(self, x, mask_ratio):
+        '''The strategy of random masking in MAE is:
+            1) Randomly (normal dist) shuffle the embedded patches on patch level.
+            2) Remove the last N patches based on the mask ratio.
+            3) Use the remaining patches as input to the encoder.
+            4) The output from the encoder will be then appended with mask tokens (the overall length of this output now will equal to the original length of the patch embedding vector).
+            5) Then, this new vector will be unshuffled back to the original positions from before 1). The result will be mask tokens at places where the original elements were removed, and MAE's encoder's output at the non-removed elements.
+            6) The decoder will take the vector from 5) as input and predict a vector of the original size input before 1).
+            7) Finally, the output from decoder will be masked so that only the unmasked patches/pixels are compared for the loss.
+        '''
+    
+    def forward_encoder(self, x, mask_ratio=None):
+        '''Forward propagation for the encoder module.
+        '''
 
+        x = self.patch_embed(x) #patch embedding as in traditional ViT.
 
+        x = x + self.encoder_pos_embed[:, 1:, :] #the cls token is not yet appended to the patch embedding. We will add the cls token after masking. Hence the pos embed is excluded of the cls token index as well.
 
 
 
